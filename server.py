98bbaf3973d490badbbca28e264a7861ac0ea113
gibran-abdillah <unkownuser145@gmail.com>
2021-09-04 18:49:22 +0700

    fixing some bugs when find partner

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, render_template
-from flask_socketio import SocketIO, emit, send
-import os 
+from flask_socketio import SocketIO, emit, join_room, send
+import os, random, string
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(10)
 
 LIST_USERS = {}
 QUEUE_USERS = []
+
+
+RANDOM_CHAR = string.ascii_letters + string.ascii_uppercase + str(string.digits)
 
 socketio = SocketIO(app, cors_allowed_origin='*')
 
@@ -27,7 +30,7 @@ def add_username(username):
         print('username dah ada')
         emit('error_username', username)
     else:
-        LIST_USERS[username] = request.sid
+        LIST_USERS[username] = ''.join(random.choice(RANDOM_CHAR) for _ in range(10))
         emit('valid_username', username)
     print(LIST_USERS)
 
@@ -36,11 +39,13 @@ def find_partner(username):
     if len(QUEUE_USERS) == 0:
         print('QUEUE ADDED ', username)
         QUEUE_USERS.append(LIST_USERS.get(username))
+        join_room(LIST_USERS.get(username))
     else:
         print('Udah ada sebelumnya ')
         filtered_queue = check_queue(username).pop()
-        print(filtered_queue)
-        emit('found_partner', namespace='/private')
+        print('ROOM ', filtered_queue)
+        join_room(filtered_queue)
+        emit('found_partner', room=filtered_queue)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
